e5ac5460e230716dc5e79cf81865927a0e498095
supapitploy <88095690+supapitploy@users.noreply.github.com>
2022-09-12 16:41:16 +0700

    Update totalPages.js

--- totalPages.js
+++ totalPages.js
@@ -2,5 +2,18 @@ const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
   //code here
+  if (arrayItems == null) {
+    return undefined;
+  }
+
+  if (rowsPerPage == null || rowsPerPage == 0) {
+    return 1;
+  }
+
+  if (arrayItems.length % rowsPerPage == 0) {
+    return arrayItems.length / rowsPerPage;
+  } else {
+    return Math.floor(arrayItems.length / rowsPerPage) + 1;
+  }
 }
 module.exports = totalPages
